1f2e0124fed13a6516c4bff433a3812ac3381ce6
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2026-03-22 01:40:50 +0000

    Text editor for shaders (closes #12)

--- src/ui/panels/inspectors/shader_inspector.rs
+++ src/ui/panels/inspectors/shader_inspector.rs
@@ -1,30 +1,57 @@
 use egui::TextEdit;
+use egui_extras::syntax_highlighting::SyntectSettings;
+use std::sync::OnceLock;
+use syntect::highlighting::ThemeSet;
+use syntect::parsing::{SyntaxDefinition, SyntaxSetBuilder};
 
 use crate::{project::ShaderId, ui::pane::StateSnapshot};
+
+const WGSL_SYNTAX: &str = include_str!("WGSL.sublime-syntax");
+
+static WGSL_SYNTAX_SETTINGS: OnceLock<SyntectSettings> = OnceLock::new();
+
+// TODO: move to state to reuse in other places
+fn wgsl_settings() -> &'static SyntectSettings {
+    WGSL_SYNTAX_SETTINGS.get_or_init(|| {
+        let mut builder = SyntaxSetBuilder::new();
+        let syntax = SyntaxDefinition::load_from_str(WGSL_SYNTAX, true, Some("wgsl"))
+            .expect("WGSL sublime-syntax definition should be valid YAML");
+        builder.add(syntax);
+        SyntectSettings {
+            ps: builder.build(),
+            ts: ThemeSet::load_defaults(),
+        }
+    })
+}
 
 impl StateSnapshot<'_> {
     pub fn shader_inspector_ui(&mut self, ui: &mut egui::Ui, shader_id: ShaderId) {
         ui.take_available_space();
         let shader = self.project.shaders.get_mut(shader_id).unwrap();
 
-        let mut no_wrap_layouter =
-            |ui: &egui::Ui, text: &dyn egui::TextBuffer, _wrap_width: f32| {
-                let text_color = ui.visuals().widgets.inactive.text_color();
-                let font_id = egui::TextStyle::Monospace.resolve(ui.style());
-                let mut layout_job = egui::text::LayoutJob::simple(
-                    text.as_str().to_owned(),
-                    font_id,
-                    text_color,
-                    f32::INFINITY,
-                );
-                layout_job.wrap.max_rows = usize::MAX;
-                ui.fonts_mut(|f| f.layout_job(layout_job))
-            };
+        let theme = egui_extras::syntax_highlighting::CodeTheme::from_memory(ui.ctx(), ui.style());
 
-        TextEdit::multiline(&mut shader.source.as_ref())
+        let mut layouter = |ui: &egui::Ui, text: &dyn egui::TextBuffer, _wrap_width: f32| {
+            let mut layout_job = egui_extras::syntax_highlighting::highlight_with(
+                ui.ctx(),
+                ui.style(),
+                &theme,
+                text.as_str(),
+                "wgsl",
+                wgsl_settings(),
+            );
+
+            layout_job.wrap.max_rows = usize::MAX;
+            ui.fonts_mut(|f| f.layout_job(layout_job))
+        };
+
+        TextEdit::multiline(&mut shader.source)
+            .font(egui::TextStyle::Monospace)
             .code_editor()
+            .desired_rows(10)
+            .lock_focus(true)
             .desired_width(f32::INFINITY)
-            .layouter(&mut no_wrap_layouter)
+            .layouter(&mut layouter)
             .show(ui);
     }
 }
